7710b78d57e4861646b24008904332a51cf7373a
bwesson123 <93096306+bwesson123@users.noreply.github.com>
2022-07-30 17:06:04 -0500

    push

---
+++ pass.py
@@ -0,0 +1,17 @@
+# all ways import lib.
+import random
+passlen = int(input("enter the length of password"))
+# I use "abc" ,"123","@#$$$
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
+
+
+
+
+
+
+#OUTPUT 
+'''
+enter the length of password 30
+h!OA@JU$f%K0FiM^dW)1Hk&Rug(t82  # NEW password gen..
